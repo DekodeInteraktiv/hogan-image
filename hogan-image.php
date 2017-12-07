ba4791f2e4017ed967d1cec33a2ad6d49a86ca20
pederan <peder1976@gmail.com>
2017-12-07 13:53:25 +0100

    updated readme and some filter names

--- hogan-image.php
+++ hogan-image.php
@@ -4,7 +4,7 @@
  * Plugin URI: https://github.com/dekodeinteraktiv/hogan-image
  * GitHub Plugin URI: https://github.com/dekodeinteraktiv/hogan-image
  * Description: Image Module for Hogan.
- * Version: 1.0.1
+ * Version: 1.0.2
  * Author: Dekode
  * Author URI: https://dekode.no
  * License: GPL-3.0
