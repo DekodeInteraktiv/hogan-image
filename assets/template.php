536f90f6f966d45f51248f37e5dd063a5aff2a28
Petter Walbø Johnsgård <petter@dekode.no>
2018-02-06 11:04:19 +0100

    Use `hogan_classnames` to join classnames, and use caption component on caption

--- assets/template.php
+++ assets/template.php
@@ -20,19 +20,12 @@ if ( empty( $this->image ) ) {
 	return;
 }
 
-$figure_classes_array   = apply_filters( 'hogan/module/image/figure_classes', [ 'wp-caption', 'size-' . $this->image['size'] ], $this );
-$figure_classes_escaped = array_map( 'esc_attr', $figure_classes_array );
-$figure_classes         = trim( implode( ' ', array_filter( $figure_classes_escaped ) ) );
+printf( '<figure class="%s">',
+	esc_attr( hogan_classnames(
+		apply_filters( 'hogan/module/image/figure_classes', [ 'wp-caption', 'size-' . $this->image['size'] ], $this )
+	) )
+);
 
-$figure_caption_classes_array   = apply_filters( 'hogan/module/image/figure_caption_classes', [ 'wp-caption-text' ], $this );
-$figure_caption_classes_escaped = array_map( 'esc_attr', $figure_caption_classes_array );
-$figure_caption_classes         = trim( implode( ' ', array_filter( $figure_caption_classes_escaped ) ) );
-
-?>
-<?php echo sprintf( '<figure%1$s>', ! empty( $figure_classes ) ? ' class="' . $figure_classes . '"' : '' );
-?>
-
-<?php
 if ( ! empty( $this->heading ) ) {
 	hogan_component( 'heading', [
 		'title' => $this->heading,
@@ -47,11 +40,11 @@ echo wp_get_attachment_image(
 );
 
 $caption = apply_filters( 'hogan/module/image/template/caption', $this->caption, $this );
+if ( ! empty( $caption ) ) {
+	hogan_component( 'caption', [
+		'classname' => apply_filters( 'hogan/module/image/figure_caption_classes', [ 'wp-caption-text' ], $this ),
+		'content'   => $caption,
+	] );
+}
 
-if ( ! empty( $caption ) ) : ?>
-	<?php echo sprintf( '<figcaption%1$s>%2$s</figcaption>',
-		! empty( $figure_caption_classes ) ? ' class="' . $figure_caption_classes . '"' : '',
-		wp_kses( $caption, hogan_caption_allowed_html() )
-	); ?>
-<?php endif; ?>
-</figure>
+echo '</figure>';
